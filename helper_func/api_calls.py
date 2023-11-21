8db2319d2d639a3c4003c26e41561129fd38c88f
Lim Wei Der <limweider@Lims-MacBook-Pro.local>
2023-11-21 19:00:44 +0800

    Finalising Getting Txns function

--- helper_func/api_calls.py
+++ helper_func/api_calls.py
@@ -6,7 +6,7 @@ import json
 import pandas as pd 
 
 def call_token_txns (chain_name, wallet_add, page_num) : 
-    
+
     url = "https://api.covalenthq.com/v1/{}/address/{}/transactions_v3/page/{}/?".format(chain_name, wallet_add, page_num)
 
     headers = {
@@ -18,25 +18,24 @@ def call_token_txns (chain_name, wallet_add, page_num) :
     response = requests.get(url, headers=headers, auth=basic)
 
     # Handle the response and return data as needed.
-    if response.status_code == 200:
-        return response.json()
-    else:
+    if response.status_code != 200:
         return {"error": response.status_code}
     
-    # convert (usually response is in non JSON form) to json object first 
     json_object = json.loads(response.text) 
 
+    data = json_object['data']
 
-    print(json_object, end_trig)
-    
-    if json_object['links']['next'] == None : 
+    if data['links']['next'] == None : 
         end_trig = 1
     else : 
         end_trig = 0
 
-     
+    # json_formatted = json.dumps(json_object, indent=2)
 
-    return json_object, end_trig
+    # with open ("sample.json", "w") as output : 
+    #     output.write(json_formatted)
+
+    return data, end_trig
 
 
 def get_all_token_txns (chain_name, contract_add, wallet_add) : 
@@ -44,25 +43,33 @@ def get_all_token_txns (chain_name, contract_add, wallet_add) :
 
     end_trig = 0
     curr_page = 0
+    df_index = 0 
 
-    columns = ['from', 'to', 'value']
+    columns = ['tx_hash', 'from', 'to', 'value']
     df = pd.DataFrame(columns=columns)
 
     while end_trig == 0 :
-        json_object, end_trig = call_token_txns(chain_name, wallet_add, curr_page)
+        data, end_trig = call_token_txns(chain_name, wallet_add, curr_page)
 
-        for item in json_object['items'] : 
+        for item in data['items'] : 
             for log in item['log_events'] :
-                if log['sender_address'] == contract_add : 
-                    for param in log['decoded']['params'] : 
+                # standardising all to lower case for comparison 
+                if log['sender_address'].lower() == contract_add.lower() : 
+                    df.loc[df_index, 'tx_hash'] = log['tx_hash']
+                    for param in log['decoded']['params'] :                         
                         if param['name'] == 'from' : 
-                            df.loc[len(df), 'from'] = param['value']
+                            df.loc[df_index, 'from'] = param['value']
                         elif param['name'] == 'to' : 
-                            df.loc[len(df), 'to'] = param['value']
+                            df.loc[df_index, 'to'] = param['value']
                         elif param['name'] == 'value' : 
-                            df.loc[len(df), 'value'] = int(param['value']) 
+                            df.loc[df_index, 'value'] = int(param['value']) / (10 ** 18) 
+                    
+                    df_index += 1
         
         curr_page += 1 
+        end_trig = 1
+
+    df.to_csv('output.csv')
 
     return df
 
--- main.py
+++ main.py
@@ -3,5 +3,10 @@ import json
 
 if __name__ == '__main__' : 
     json_object = get_all_token_txns('eth-mainnet', '0xb23d80f5FefcDDaa212212F028021B41DEd428CF', '0x1c0a684a45C8AEc7C6Bd6Da94bfaED044EC2Fd88')
+    
+    # json_formatted = json.dumps(json_object, indent=2)
 
-    print(json_object)
+    # with open ("sample.json", "w") as output : 
+    #     output.write(json_formatted)
+
+
